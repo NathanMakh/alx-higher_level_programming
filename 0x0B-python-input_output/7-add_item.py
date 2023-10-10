778c252ca38800736bb5a65df6d85685384c2414
NathanMakh <106968587+NathanMakh@users.noreply.github.com>
2023-10-10 10:25:05 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""
-Module 7-save_to_json_file
+import sys
+import json
 
-Contains function that writes Python obj to file using JSON represenation
-"""
-
-
+# Function to save a Python object to a JSON file
 def save_to_json_file(my_obj, filename):
-    """Writes Python obj to file using JSON represenation
-    Args:
-        my_obj: python object
-        filename: file
-    """
-    import json
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
 
-    with open(filename, mode="w", encoding="utf-8") as f:
-        json.dump(my_obj, f)
+# Function to load a Python object from a JSON file
+def load_from_json_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+# Check if the file exists, and load its content if it does
+try:
+    existing_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    existing_list = []
+
+# Add command-line arguments to the list
+existing_list.extend(sys.argv[1:])
+
+# Save the updated list to the JSON file
+save_to_json_file(existing_list, 'add_item.json')
