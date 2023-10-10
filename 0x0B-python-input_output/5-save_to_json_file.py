afd61b67363bded0fdb122a9aa869b36208d81eb
NathanMakh <106968587+NathanMakh@users.noreply.github.com>
2023-10-10 10:22:29 +0300

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
-"""
-Module 5-to_json_string
+import json
 
-Contains function that returns JSON representation of obj (string)
-"""
-
-
-def to_json_string(my_obj):
-    """Returns JSON representation of obj (string)
-    Args:
-        my_obj: python object
-    Return:
-        json string representation
-    """
-    import json
-
-    return json.dumps(my_obj)
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
