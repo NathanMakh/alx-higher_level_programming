6bcdd421d873a549736a19bc4067970141cf767b
NathanMakh <106968587+NathanMakh@users.noreply.github.com>
2023-10-10 10:31:56 +0300

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
-"""
-Module 11-student
-
-Contains class Student
-that initializes public instance attributes first_name, last_name, and age,
-and has public method to_json that retrieves its dictionary representation
-"""
-
-
-class Student():
-    """
-    Public Attributes:
-        first_name
-        last_name
-        age
-
-    Public Methods:
-        to_json: retrieves its dictionary representation
-    """
+class Student:
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes student with full name and age
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """
-        Returns dictionary description with simple data structure
-        (list, dictionary, dictionary, string)
-        for JSON serialization of an object
-        """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
+
+# Example usage:
+if __name__ ==
